088ca0350bbcafd355fb8ce9da24dd7d61b34a03
antoncp <antoncp@gmail.com>
2023-08-13 15:57:00 +0200

    All admin models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Recipe, Ingredient, Tag, RecipeIngredient
+
+
+class IngredientInline(admin.TabularInline):
+    model = RecipeIngredient
 
 
 class RecipeAdmin(admin.ModelAdmin):
@@ -8,6 +12,25 @@ class RecipeAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     list_filter = ('name',)
     empty_value_display = '-empty-'
+    inlines = [
+        IngredientInline,
+    ]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-empty-'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'color', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-empty-'
 
 
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,6 +4,44 @@ from django.db import models
 User = get_user_model()
 
 
+class Tag(models.Model):
+    """Tag db model class."""
+
+    name = models.CharField(
+        'Name',
+        max_length=256,
+        unique=True,
+    )
+    color = models.CharField(
+        'Color',
+        max_length=16
+    )
+    slug = models.SlugField(
+        'Slug',
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient db model class."""
+
+    name = models.CharField(
+        'Name',
+        max_length=256
+    )
+    measurement_unit = models.CharField(
+        'Measurement unit',
+        max_length=256
+    )
+
+    def __str__(self):
+        return self.name
+    
+
 class Recipe(models.Model):
     """Recipe db model class."""
 
@@ -22,7 +60,21 @@ class Recipe(models.Model):
     )
     cooking_time = models.PositiveSmallIntegerField(
         'Cooking time',
-    )   
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        null=True,
+        default=None
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Tag',
+        related_query_name='Recipes'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through="RecipeIngredient"
+    )
 
     class Meta:
         verbose_name = 'Recipe'
@@ -30,3 +82,26 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RecipeIngredient(models.Model):
+    """Ingredients in recipe db model class."""
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="Ingredients",
+        verbose_name="Recipe",
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="Recipes",
+        verbose_name="Ingredient",
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Amount',
+    )
+
+    def __str__(self):
+        return f'{self.amount} of {self.ingredient} in {self.recipe}'
