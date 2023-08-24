1d219cdcfc0e4674ef25e738aa5331d2d793d5d4
antoncp <antoncp@gmail.com>
2023-08-24 15:18:31 +0200

    Adds PDF export function

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,23 @@
+import io
+
 from api.filters import IngredientSearchFilter, RecipeFilter
 from api.paginator import RecipesPagination
 from api.permissions import IsAdminOrReadOnly, IsOwnerAdminOrReadOnly
 from api.serializers import (CartRecipeSerializer, FavoriteRecipeSerializer,
                              IngredientSerializer, RecipeSerializer,
-                             TagSerializer)
+                             TagSerializer, FollowSerializer)
+from django.db.models import Sum
+from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import CartRecipe, FavoriteRecipe, Ingredient, Recipe, Tag
+from recipes.models import (CartRecipe, FavoriteRecipe, Ingredient, Recipe,
+                            RecipeIngredient, Tag)
+from users.models import Follow
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import action
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
@@ -77,6 +86,52 @@ class RecipeViewSet(viewsets.ModelViewSet):
         model_obj.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=[IsAuthenticated],
+    )
+    def download_shopping_cart(self, request):
+        buffer = io.BytesIO()
+        p = canvas.Canvas(buffer)
+        pdfmetrics.registerFont(
+            TTFont("FreeSans", "foodgram_static/freesans.ttf", "UTF-8")
+        )
+        p.setFont("FreeSans", 24)
+        p.setFillColorRGB(0, 0, 255)
+        p.drawString(50, 770, "ИНГРЕДИЕНТЫ для покупки")
+        p.setFillColorRGB(0, 0, 0)
+        p.setFont("FreeSans", 14)
+        p.drawString(50, 750, "(на основе рецептов в вашей корзине)")
+        p.line(10, 725, 550, 725)
+        ingredients = (
+            RecipeIngredient.objects.filter(
+                recipe__ShoppingCart__user=request.user
+            )
+            .values("ingredient__name", "ingredient__measurement_unit")
+            .annotate(total_amount=Sum("amount"))
+            .order_by("-total_amount")
+        )
+        pos_y = 700
+        for num, ingredient in enumerate(ingredients, start=1):
+            p.drawString(
+                50,
+                pos_y,
+                (
+                    f"{num}) {ingredient['ingredient__name']} "
+                    f"{ingredient['total_amount']} "
+                    f"{ingredient['ingredient__measurement_unit']}"
+                ),
+            )
+            pos_y -= 25
+        p.line(10, pos_y, 550, pos_y)
+        p.showPage()
+        p.save()
+        buffer.seek(0)
+        return FileResponse(
+            buffer, as_attachment=True, filename="shopping-list.pdf"
+        )
+
 
 class TagViewSet(viewsets.ModelViewSet):
     """Tags API endpoint."""
@@ -98,3 +153,15 @@ class IngredientViewSet(viewsets.ModelViewSet):
     pagination_class = None
     filter_backends = (IngredientSearchFilter,)
     search_fields = ("^name",)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    """Follow API endpoint."""
+
+    http_method_names = ["get"]
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.following.all()
