909f689c543e06228cbb3ecef85c06f6eb55f865
antoncp <antoncp@gmail.com>
2023-08-27 14:44:08 +0200

    Fix PEP8 in load_csv.py

--- backend/recipes/management/commands/load_csv.py
+++ backend/recipes/management/commands/load_csv.py
@@ -8,17 +8,17 @@ class Command(BaseCommand):
     help = "Loads data from .csv files to the DB"
 
     def add_arguments(self, parser):
-        parser.add_argument('path')
-        parser.add_argument('model')
+        parser.add_argument("path")
+        parser.add_argument("model")
 
     def handle(self, *args, **options):
-        path = options.get('path')
-        model = apps.get_model('recipes', options.get('model'))
+        path = options.get("path")
+        model = apps.get_model("recipes", options.get("model"))
 
-        with open(path, 'r', encoding='utf-8') as file:
+        with open(path, "r", encoding="utf-8") as file:
             for row in DictReader(file):
                 record = model(**row)
                 record.save()
-                self.stdout.write(f'Added to a database: {row}')
+                self.stdout.write(f"Added to a database: {row}")
 
-            self.stdout.write('Import to a database finished')
+            self.stdout.write("Import to a database finished")
